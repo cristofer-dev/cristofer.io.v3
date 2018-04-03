b911210a6fc662b18fc80193c82b91e571685c77
Cristofer [HP] <cristofer.dev@gmail.com>
2018-04-02 22:47:04 -0300

    SET Header and buttons with css grid
     Changes to be committed:
    	modified:   src/components/Header/index.js
    	modified:   src/layouts/base.css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import logo from '../../assets/img/logo.png'
+
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+  <div className='header'>
+    <div className='menu' >
+      <div className="wrapper">
+        <div className="one">
+          <Link to="/">
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="btn two">blog</div>
+        <div className="btn three">repos</div>
+      </div>
     </div>
   </div>
 )
