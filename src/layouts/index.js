1c5b9bb4df62502eb00f32d26071dd2682d112b2
Cristofer [HP] <cristofer.dev@gmail.com>
2018-04-09 19:52:38 -0300

    UPDATE CSS
     Changes to be committed:
    	modified:   src/layouts/base.css
    	modified:   src/layouts/index.css
    	modified:   src/layouts/index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,8 +4,8 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import './index.css'
-import './base.css'
 import '../assets/css/prism-default.css';
+import './base.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
