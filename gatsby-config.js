2c97eed6ff03e4cb2beaa22e382629ac992872ca
Cristofer [HP] <cristofer.dev@gmail.com>
2018-04-13 23:16:22 -0300

    ADD plugins for PWA
     Changes to be committed:
    	modified:   gatsby-config.js
    	modified:   package.json
    	new file:   src/assets/icons/icon.svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,38 +13,51 @@ module.exports = {
       },
     },
     {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            // Class prefix for <pre> tags containing syntax highlighting;
-            // defaults to 'language-' (eg <pre class="language-js">).
-            // If your site loads Prism into the browser at runtime,
-            // (eg for use with libraries like react-live),
-            // you may use this to prevent Prism from re-processing syntax.
-            // This is an uncommon use-case though;
-            // If you're unsure, it's best to use the default value.
-            classPrefix: "language-",
-            // This is used to allow setting a language for inline code
-            // (i.e. single backticks) by creating a separator.
-            // This separator is a string and will do no white-space
-            // stripping.
-            // A suggested value for English speakers is the non-ascii
-            // character '›'.
-            inlineCodeMarker: null,
-            // This lets you set up language aliases.  For example,
-            // setting this to '{ sh: "bash" }' will let you use
-            // the language "sh" which will highlight using the
-            // bash highlighter.
-            aliases: {},
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              // Class prefix for <pre> tags containing syntax highlighting;
+              // defaults to 'language-' (eg <pre class="language-js">).
+              // If your site loads Prism into the browser at runtime,
+              // (eg for use with libraries like react-live),
+              // you may use this to prevent Prism from re-processing syntax.
+              // This is an uncommon use-case though;
+              // If you're unsure, it's best to use the default value.
+              classPrefix: "language-",
+              // This is used to allow setting a language for inline code
+              // (i.e. single backticks) by creating a separator.
+              // This separator is a string and will do no white-space
+              // stripping.
+              // A suggested value for English speakers is the non-ascii
+              // character '›'.
+              inlineCodeMarker: null,
+              // This lets you set up language aliases.  For example,
+              // setting this to '{ sh: "bash" }' will let you use
+              // the language "sh" which will highlight using the
+              // bash highlighter.
+              aliases: {},
+            },
           },
-        },
-      ],
+        ],
+      },
     },
-  },
-    
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Cristofer.io",
+        short_name: "Cristofer",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icon: "src/assets/icons/icon.svg" // This path is relative to the root of the site.
+      },
+    }
+
   ],
 
 };
